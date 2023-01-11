3af7ee5c260ae6b0a9f49b7b5716c31b6d1f8f2e
vasil.krumov <vassil.krumov@gmail.com>
2023-01-11 17:08:17 +0200

    🎉 Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Awesome</Text>
+            <Text>Sweet</Text>
+            <Button title="Tap Me!" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
